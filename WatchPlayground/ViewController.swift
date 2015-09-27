a6a5b04c362097266fa1561192b7593ca4092ce1
Manu Kanthan <m.kanthan@gmail.com>
2015-09-26 20:59:50 -0400

    Initial Commit

---
+++ WatchPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchPlayground
+//
+//  Created by Manu Kanthan on 9/26/15.
+//  Copyright © 2015 Manu Kanthan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
